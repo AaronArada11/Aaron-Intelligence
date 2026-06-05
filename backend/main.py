33ceb5cf374278a3bf98683a81ef51e625bb80cb
Aaron Arada <aaronarada011@gmail.com>
2026-06-05 17:10:52 +0800

    Added Needed Libraries and Added Gemini

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 app = FastAPI()
 
+
+class ChatRequest(BaseModel):
+    message: str
+
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Aaron Intelligence is running"}
+
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = model.generate_content(request.message)
+
+    return {
+        "answer": response.text
+    }
